14b62527c6fd85974485a8ce438e8988220e77d4
J2PIn <64399278+J2PIn@users.noreply.github.com>
2026-02-11 09:57:52 +0200

    Add Home component with navigation link

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ maxWidth: 980, margin: "0 auto", padding: 16 }}>
+      <h1>Ehdokas</h1>
+      <p><a href="#/disclose">Go to /disclose</a></p>
+    </div>
+  );
+}
