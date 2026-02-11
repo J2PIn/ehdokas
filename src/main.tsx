888e70fca61968787fcd23f31e2deaa45fe3582f
J2PIn <64399278+J2PIn@users.noreply.github.com>
2026-02-11 09:53:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./ehdokas.css";
+import "./index.css";
+import Home from "./pages/Home";
+import Disclose from "./pages/Disclose";
+
+function route() {
+  const h = window.location.hash || "#/";
+  if (h.startsWith("#/disclose")) return "disclose";
+  return "home";
+}
+
+function App() {
+  const [r, setR] = React.useState(route());
+
+  React.useEffect(() => {
+    const onHash = () => setR(route());
+    window.addEventListener("hashchange", onHash);
+    return () => window.removeEventListener("hashchange", onHash);
+  }, []);
+
+  return r === "disclose" ? <Disclose /> : <Home />;
+}
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
