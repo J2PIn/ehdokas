149aa8f559d80757aaa322184b9f25085b5e743a
J2PIn <64399278+J2PIn@users.noreply.github.com>
2026-02-14 13:16:05 +0200

    Replace Home component with SiteHome

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,5 @@
+import SiteHome from "../App";
+
 export default function Home() {
-  return (
-    <div style={{ maxWidth: 980, margin: "0 auto", padding: 16 }}>
-      <h1>Ehdokas</h1>
-      <p><a href="#/disclose">Go to /disclose</a></p>
-    </div>
-  );
+  return <SiteHome />;
 }
